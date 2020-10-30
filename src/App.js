8af90bfb6a369b9af1058480f87132c35bbbeddf
Chinmay Parab <43921132+chinmayparab@users.noreply.github.com>
2020-10-30 13:23:32 +0530

    Auth Components | Styled Auth Buttons | Success Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
+import React, { Component } from "react";
 import "./App.css";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-function App() {
-  return <div className='App'>Firebase Auth</div>;
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  });
+}
+class App extends Component {
+  state = { isSignedIn: false };
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ isSignedIn: !!user });
+      console.log("user", user);
+    });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.isSignedIn ? (
+          <span>
+            <h1>EID Mubarak {firebase.auth().currentUser.displayName}</h1>
+            {firebase.auth().currentUser.photoURL && (
+              <img alt='profile' src={firebase.auth().currentUser.photoURL} />
+            )}
+            <br />
+            <br />
+            <br />
+            <br />
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+          </span>
+        ) : (
+          <>
+            <h1>Sign In!</h1>
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
